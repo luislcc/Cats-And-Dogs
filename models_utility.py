bc9585edafc95aff1077943436260385e682e61d
João Pedro M. M. C. Antunes <76684399+MrXester@users.noreply.github.com>
2022-11-03 18:51:05 +0000

    possibility to star DA empty

--- models_utility.py
+++ models_utility.py
@@ -3,13 +3,14 @@ import numpy as np
 import copy
 
 class DataAugmentator(object):
-	def __init__(self, **kwargs):
-		self.width_shift_range= 0.3
-		self.height_shift_range= 0.3
-		self.horizontal_flip= True
-		self.rotation_range= 90
-		self.zoom_range= [0.5,1.5]
-		self.noise = True
+	def __init__(self, empty=False, **kwargs):
+		if not empty:
+			self.width_shift_range= 0.3
+			self.height_shift_range= 0.3
+			self.horizontal_flip= True
+			self.rotation_range= 90
+			self.zoom_range= [0.5,1.5]
+			self.noise = True
 		self.__dict__.update(**kwargs)
 
 	def add_noise(img):
